c96c9238323b53204e7c83c978a43ebb2a89b534
emanabuwaked <emanabuwaked@gmail.com>
2020-06-30 11:05:03 +0300

    hi

--- server/db/config.js
+++ server/db/config.js
@@ -1,45 +1,70 @@
+var Mongo = require('mongodb')
 const Promise = require('bluebird');
+
 
 module.exports = (db) => {
   if (!db.queryAsync) {
     db = Promise.promisifyAll(db);
   }
-  // Create links table
-  return db.queryAsync(`
-    CREATE TABLE IF NOT EXISTS links (
-      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      url VARCHAR(255),
-      baseUrl VARCHAR(255),
-      code VARCHAR(5),
-      title VARCHAR(255),
-      visits INT NOT NULL DEFAULT 0
-    );`)
+  ////Create links table
+  // return db.queryAsync(`
+  //   CREATE TABLE IF NOT EXISTS links (
+  //     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  //     url VARCHAR(255),
+  //     baseUrl VARCHAR(255),
+  //     code VARCHAR(5),
+  //     title VARCHAR(255),
+  //     visits INT NOT NULL DEFAULT 0
+  //   );`)
+      return db.createCollection(IF NOT EXISTS links,{
+        id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        url: VARCHAR(255),
+        baseUrl: VARCHAR(255),
+        code: VARCHAR(5),
+        title: VARCHAR(255),
+        visits: INT NOT NULL DEFAULT 0
+      })
     .then(() => {
-      // Create clicks table
-      return db.queryAsync(`
-        CREATE TABLE IF NOT EXISTS clicks (
-          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-          linkId INT
-        );`);
+      //// Create clicks table
+      // return db.queryAsync(`
+      //   CREATE TABLE IF NOT EXISTS clicks (
+      //     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      //     linkId INT
+      //   );`);
+      return db.createCollection(IF NOT EXISTS clicks,{
+        id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        linkId: INT
+      })
     })
     .then(() => {
-      // Create users table
-      return db.queryAsync(`
-        CREATE TABLE IF NOT EXISTS users (
-          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-          username VARCHAR(40) UNIQUE,
-          password VARCHAR(64),
-          salt VARCHAR(64)
-        );`);
+      // // Create users table
+      // return db.queryAsync(`
+      //   CREATE TABLE IF NOT EXISTS users (
+      //     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      //     username VARCHAR(40) UNIQUE,
+      //     password VARCHAR(64),
+      //     salt VARCHAR(64)
+      //   );`);
+      return db.createCollection(IF NOT EXISTS users,{
+        id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        username: VARCHAR(40) UNIQUE,
+        password VARCHAR(64),
+        salt VARCHAR(64)
+      })
     })
     .then(() => {
-      // Create sessions table
-      return db.queryAsync(`
-        CREATE TABLE IF NOT EXISTS sessions (
-          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-          hash VARCHAR(64),
-          userId INT
-        )`);
+      // // Create sessions table
+      // return db.queryAsync(`
+      //   CREATE TABLE IF NOT EXISTS sessions (
+      //     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      //     hash VARCHAR(64),
+      //     userId INT
+      //   )`);
+      return db.createCollection(IF NOT EXISTS clicks,{
+        id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        hash: VARCHAR(64),
+        userId: INT
+      })
     })
     .error(err => {
       console.log(err);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app.js');
 const db = require('./db');
-const port = 4568;
+const port = process.env.PORT || 4568;
 
 app.listen(port, () => {
   console.log(`Shortly is listening on ${port}`);
